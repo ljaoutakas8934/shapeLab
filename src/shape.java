4e34b53d94eb5b2c24a0fdb29ef0c095f3dad8d5
BT_1N3_26 <ljaoutakas8934@bths.edu>
2018-10-16 14:29:04 -0400

    commit 2

---
+++ src/shape.java
@@ -0,0 +1,18 @@
+public abstract class shape
+{
+    private String type;
+    public shape (String type)
+    {
+        this.type = type;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public String toString()
+    {
+        return ("I am a " + type + ".");
+    }
+
+}
