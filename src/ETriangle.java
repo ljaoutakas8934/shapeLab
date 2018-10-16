3d787ed5e9ab7a5c722b307d4d127e4c7cd11202
Lintmaker <lpan7932@bths.edu>
2018-10-16 14:49:10 -0400

    Started

--- src/ETriangle.java
+++ src/ETriangle.java
@@ -1,3 +1,16 @@
-public class ETriangle
-{
-}
+  public class ETriangle extends Shape
+    {
+        private int side;
+        public ETriangle(int side)
+        {
+            super("ETriangle");
+            this.side = side;
+        }
+        @Override
+        public double getArea()
+        {
+            return
+        }
+    }
+
+
