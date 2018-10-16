a51a266724ec4c0a24d6f0545f84b9b8c812f964
BT_1N3_27 <hkaur0745@bths.edu>
2018-10-16 14:46:02 -0400

    rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,3 +1,18 @@
-public class Rectangle
+public class Rectangle extends Shape
 {
+    private int side;
+    public Rectangle(int side)
+    {
+        super("Rectangle");
+        this.side = side;
+    }
+    public double getArea()
+    {
+        return (side*side);
+    }
+
+    public double getPerimeter()
+    {
+        return side+side+side+side;
+    }
 }
