a17f999752113f652724fea9c07fca97e38d9eb7
BT_1N3_25 <lpan7932@bths.edu>
2018-10-17 14:47:06 -0400

    Triangle confirmed

--- src/ETriangle.java
+++ src/ETriangle.java
@@ -1,16 +1,20 @@
-  public class ETriangle extends Shape
+public class ETriangle extends Shape
+{
+    private int side;
+    public ETriangle(int side)
     {
-        private int side;
-        public ETriangle(int side)
-        {
-            super("ETriangle");
-            this.side = side;
-        }
-        @Override
-        public double getArea()
-        {
-            return
-        }
+        super("ETriangle");
+        this.side = side;
+    }
+    @Override
+    public double getArea()
+    {
+        return (Math.sqrt(3)/4) * side * side;
     }
 
-
+    @Override
+    public double getPerimeter()
+    {
+        return side * 3;
+    }
+}
