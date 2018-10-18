702b11f5e9d96fbcb21cd8b120c48031dbf5c384
BT_1N3_27 <hkaur09745@bths.edu>
2018-10-18 14:48:03 -0400

    updated1

---
+++ src/Sqaure.java
@@ -0,0 +1,32 @@
+public class extend Sqaure {
+    private int side ;
+    /**
+     * initializes the parameters of the rectangle
+     * @param side side length 1
+     */
+    public Square(int side)
+    {
+        super("Square");
+        this.side = side;
+    }
+
+    /**
+     * multiplies side by itself because they are equivalent in
+     * @return returns product of side lengths which is area
+     */
+    public double getArea()
+    {
+
+        return (side*side);
+    }
+
+    /**
+     * multiplies the side by 4 to essentially calculate the sum of the side lengths
+     * @return returns perimeter of a square
+     */
+    public double getPerimeter()
+    {
+        return (4*side);
+    }
+}
+
