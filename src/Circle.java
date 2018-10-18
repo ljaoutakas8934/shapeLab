87e6d3375ec88c0dee5dfda2c71bb6a7654a0baf
BT_1N3_26 <ljaoutakas8934@bths.edu>
2018-10-18 14:35:55 -0400

    commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,20 +1,37 @@
+/**
+ * Author: Loris Jautakas
+ * This class is used to create a circle with a radius that can be inputted.
+ */
 public class Circle extends Shape
 {
     private int radius;
 
+    /**
+     *  This function sets the value of the private int radius to the radius parameter that can be inputted.
+     * @param radius The radius parameter is what is changed in the runner class.
+     */
     public Circle (int radius)
     {
         super("Circle");
         this.radius = radius;
     }
     @Override
+    /**
+     * This method uses the radius established previously to get the area of a circle.
+     * @return Returns the area of a circle.
+     */
     public double getArea()
     {
         return (Math.PI*Math.pow(radius,2));
     }
 
     @Override
-    public double getPerimeter() {
+    /**
+     * This method finds the circumference of a circle with a given radius, established previously.
+     * @return Returns the circumference of a circle.
+     */
+    public double getPerimeter()
+    {
         return Math.PI*(radius*2);
     }
 }
