587f3fc49048d7647a5badf7fe7a65d3c7b85314
BT_1N3_27 <hkaur09745@bths.edu>
2018-10-18 14:34:52 -0400

    updated1

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,22 +1,37 @@
 /**
- * Creates a rectangle with two different side lengths and calculates the area by multiplying them and perimeter by adding all four sides
+ * Creates a rectangle with two different side lengths of the name height and width
  */
 public class Rectangle extends Shape
 {
     private int height ;
     private int width;
+
+    /**
+     * initializes the parameters of the rectangle
+     * @param height side length 1
+     * @param width  side length 2
+     */
     public Rectangle(int height, int width)
     {
         super("Rectangle");
         this.height = height;
         this.width  = width;
     }
+
+    /**
+     * multiplies the two side lengths to obtain area
+     * @return returns product of side lengths which is area
+     */
     public double getArea()
     {
 
         return (height*width);
     }
 
+    /**
+     * adds the side lengths to obtain perimeter
+     * @return returns sum of all four side lengths
+     */
     public double getPerimeter()
     {
         return (2*height)+(2*width);
