c843a0f5ff9dc84169b6507f9cb5172b624eeb7f
BT_1N3_25 <lpan7932@bths.edu>
2018-10-18 14:36:43 -0400

    Added comments

--- src/ETriangle.java
+++ src/ETriangle.java
@@ -1,18 +1,29 @@
+// Author: Lin Yao Pan
+
 public class ETriangle extends Shape
 {
     private int side;
+
     public ETriangle(int side)
     {
         super("ETriangle");
         this.side = side;
     }
     @Override
+    /**
+     * Takes the value from side in parent class and returns area of the triangle based on the value.
+     * @return Returns the area of an ETriangle based on side's length
+     */
     public double getArea()
     {
         return (Math.sqrt(3)/4) * side * side;
     }
 
     @Override
+    /**
+     *  Takes the value from side in parent class and returns perimeter of the triangle based on the value.
+     * @return Returns the perimeter of an ETriangle based on side's length.
+     */
     public double getPerimeter()
     {
         return side * 3;
